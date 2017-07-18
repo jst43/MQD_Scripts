ace050781dda03e5a7d95b38f13653514303ee63
JSThompson <jst43@cam.ac.uk>
2017-07-18 17:22:21 +0100

    Fixed filepaths, added header as constant

--- Haloplex/Scripts/Haloplex_IndelCaller.sh
+++ Haloplex/Scripts/Haloplex_IndelCaller.sh
@@ -6,6 +6,7 @@ hg38Dict="/media/eguz/darwin/Resources/hg38.p6/hg38_2MergeAll.dict"
 TABLE_ANNOVAR="/media/eguz/darwin/Resources/Software/annovar/table_annovar.pl"
 humandb="/media/eguz/darwin/Resources/Software/annovar/humandb"
 PICARD="/media/eguz/darwin/Resources/Software/picard-tools-1.141/picard.jar"
+header="/home/joe/MQD_Scripts/Haloplex/Dependent_Files/header.txt"
 
 #COMMANDLINE VARIABLES
 while getopts "fh" opt; do
@@ -58,34 +59,37 @@ cd $filepath
 mkdir pindeltemp
 mkdir pindel_anno
 
+cd pindeltemp
+
 #Create list of name samples
-ls ./recal_bam/*.sorted.realigned.recal.bam > ./pindeltemp/samplesPindel.txt
-sed -i 's|.sorted.realigned.recal.bam||' ./pindeltemp/samplesPindel.txt
+ls ../recal_bam/*.recal.bam > samplesPindel.txt
+sed -i 's|../recal_bam/||' samplesPindel.txt
+sed -i 's|.sorted.realigned.recal.bam||' samplesPindel.txt
 #Create files: pindelinput.txt
 for k in `cat samplesPindel.txt`; do
-	ls ./recal_bam/${k}.sorted.realigned.recal.bam > ./pindeltemp/${k}_pindelinput.txt
-	sed -i 's|\(.\+\)\.sorted.realigned.recal.bam|\1\.sorted.realigned.recal.bam\t350\t\1|' ./pindeltemp/${k}_pindelinput.txt
+	ls ../recal_bam/${k}.sorted.realigned.recal.bam > ${k}_pindelinput.txt
+	sed -i 's|\(.\+\)\.sorted.realigned.recal.bam|\1\.sorted.realigned.recal.bam\t350\t\1|' ${k}_pindelinput.txt
 done
 
 #Run Pindel
 for i in `cat samplesPindel.txt`; do
-	pindel -f ${hg38} -i ./pindeltemp/${i}_pindelinput.txt -c ALL -o ./pindeltemp/${i}
+	pindel -f ${hg38} -i ${i}_pindelinput.txt -c ALL -o ${i}
 #Create vcf files of deletion and insertion out of pindel
-	pindel2vcf -p ./pindeltemp/${i}_D -r ${hg38} -R GRCh38 -d 201312 -G -v ./pindeltemp/${i}_D.vcf
-	pindel2vcf -p ./pindeltemp/${i}_SI -r ${hg38} -R GRCh38 -d 201312 -G -v ./pindeltemp/${i}_SI.vcf
+	pindel2vcf -p ${i}_D -r ${hg38} -R GRCh38 -d 201312 -G -v ${i}_D.vcf
+	pindel2vcf -p ${i}_SI -r ${hg38} -R GRCh38 -d 201312 -G -v ${i}_SI.vcf
 #Merging both duplicates and filtering vcf
-        bgzip -c ./pindeltemp/${i}_D.vcf > ./pindeltemp/${i}_D.vcf.gz
-        tabix -p vcf ./pindeltemp/${i}_D.vcf.gz
-        bgzip -c ./pindeltemp/${i}_SI.vcf > ./pindeltemp/${i}_SI.vcf.gz
-        tabix -p vcf ./pindeltemp/${i}_SI.vcf.gz
-        vcftools_0.1.13 --vcf ./pindeltemp/${i}_SI.vcf --max-missing 1 --recode --out ./pindeltemp/${i}_SI_F1
+        bgzip -c ${i}_D.vcf > ${i}_D.vcf.gz
+        tabix -p vcf ${i}_D.vcf.gz
+        bgzip -c ${i}_SI.vcf > ${i}_SI.vcf.gz
+        tabix -p vcf ${i}_SI.vcf.gz
+        vcftools_0.1.13 --vcf ${i}_SI.vcf --max-missing 1 --recode --out ${i}_SI_F1
 #Merging Deletion and Insertion
-	vcf-concat ./pindeltemp/${i}_D.vcf.gz ./pindeltemp/${i}_SI.vcf.gz > ./pindeltemp/${i}_DSI.vcf
-	java -jar ${PICARD} SortVcf I=./pindeltemp/${i}_DSI.vcf O=./pindeltemp/${i}_DSI.sorted.vcf SD=${hg38Dict}
-	less ./pindeltemp/${i}_DSI.sorted.vcf | grep -v "#" > ./pindeltemp/${i}_headless.vcf
-        cat header.txt ./pindeltemp/${i}_headless.vcf > ./pindeltemp/${i}_NewHead.vcf
+	vcf-concat ${i}_D.vcf.gz ${i}_SI.vcf.gz > ${i}_DSI.vcf
+	java -jar ${PICARD} SortVcf I=${i}_DSI.vcf O=${i}_DSI.sorted.vcf SD=${hg38Dict}
+	less ${i}_DSI.sorted.vcf | grep -v "#" > ${i}_headless.vcf
+        cat $header ${i}_headless.vcf > ${i}_NewHead.vcf
 #Annotation hg38
-	perl ${TABLE_ANNOVAR} ./pindeltemp/${i}_NewHead.vcf ${humandb} -buildver hg38 -out ./pindel_anno/${i}_D+SI.myanno -remove -protocol refGene,cytoBand,genomicSuperDups,esp6500siv2_all,1000g2015aug_all,1000g2015aug_afr,1000g2015aug_eas,1000g2015aug_eur,avsnp144,cosmic70,clinvar_20160302,ljb26_all -operation g,r,r,f,f,f,f,f,f,f,f,f -nastring . -vcfinput
+	perl ${TABLE_ANNOVAR} ${i}_NewHead.vcf ${humandb} -buildver hg38 -out ../pindel_anno/${i}_D+SI.myanno -remove -protocol refGene,cytoBand,genomicSuperDups,esp6500siv2_all,1000g2015aug_all,1000g2015aug_afr,1000g2015aug_eas,1000g2015aug_eur,avsnp144,cosmic70,clinvar_20160302,ljb26_all -operation g,r,r,f,f,f,f,f,f,f,f,f -nastring . -vcfinput
 done
 
 echo "lympSeq_Indels analysis was completed."
