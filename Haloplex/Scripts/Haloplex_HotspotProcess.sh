bfb36d23f5377a85a12bf39c8bce125756ebc3dc
JSThompson <jst43@cam.ac.uk>
2018-04-13 16:43:06 +0100

    Added filepath variable to mkdir command, changed variable name in while read loop

--- Haloplex/Scripts/Haloplex_HotspotProcess.sh
+++ Haloplex/Scripts/Haloplex_HotspotProcess.sh
@@ -35,9 +35,9 @@ if [ ! -d $filepath ]; then
 	exit 1
 fi
 
-mkdir recal_bam
+mkdir ${filepath}recal_bam
 
-while read pfx; do
+while read nolane; do
 	$java -Xmx40g -jar $GATKv3_5 -nct 20 -T BaseRecalibrator -R $hg38 -I ${filepath}tempfiles/${nolane}.sorted.bam -l info -knownSites $All -o ${filepath}tempfiles/${nolane}.sorted.table
 	$java -Xmx40g -jar $GATKv3_5 -nct 20 -T PrintReads -R $hg38 -I ${filepath}tempfiles/${nolane}.sorted.bam -l INFO -BQSR ${filepath}tempfiles/${nolane}.sorted.table -o ${filepath}recal_bam/${nolane}.sorted.recal.bam
 done <${filepath}samples_noLane.txt
