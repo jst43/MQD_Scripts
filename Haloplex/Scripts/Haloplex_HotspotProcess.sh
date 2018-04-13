b63c2c9047514e736fdba5c8749ac7a4a0b5cb7c
JSThompson <jst43@cam.ac.uk>
2018-04-13 15:47:59 +0100

    Made variable paths relative, added java variable path

--- Haloplex/Scripts/Haloplex_HotspotProcess.sh
+++ Haloplex/Scripts/Haloplex_HotspotProcess.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-GATK_v3_8="/mnt/raid/Resources/Software/GenomeAnalysisTK.jar"
-GATK_v3_5="/home/joe/GenomeAnalysisTK.jar"
-hg38="/mnt/raid/Resources/hg38.p6/hg38_2MergeAll.fa"
-All="/mnt/raid/Resources/hg38.p6/All.vcf"
-dbsnp="/mnt/raid/Resources/hg38.p6/All.vcf"
-TABLE_ANNOVAR="/mnt/raid/Resources/Software/annovar/table_annovar.pl"
-humandb="/mnt/raid/Resources/Software/annovar/humandb/"
+java="../../../Software/jre1.8.0_112/bin/java"
+GATKv3_5="../../../Software/GenomeAnalysisTK_v3.5.jar"
+hg38="../../../hg38.p6/hg38_2MergeAll.fa"
+All="../../..//hg38.p6/All.vcf"
 
 #COMMANDLINE VARIABLES
 while getopts "f:h" opt; do
@@ -41,6 +38,6 @@ fi
 mkdir recal_bam
 
 while read pfx; do
-	java -Xmx40g -jar $GATKv3_5 -nct 20 -T BaseRecalibrator -R $hg38 -I ${filepath}tempfiles/${nolane}.sorted.bam -l info -knownSites $All -o ${filepath}tempfiles/${nolane}.sorted.table
-	java -Xmx40g -jar $GATKv3_5 -nct 20 -T PrintReads -R $hg38 -I ${filepath}tempfiles/${nolane}.sorted.bam -l INFO -BQSR ${filepath}tempfiles/${nolane}.sorted.table -o ${filepath}recal_bam/${nolane}.sorted.recal.bam
+	$java -Xmx40g -jar $GATKv3_5 -nct 20 -T BaseRecalibrator -R $hg38 -I ${filepath}tempfiles/${nolane}.sorted.bam -l info -knownSites $All -o ${filepath}tempfiles/${nolane}.sorted.table
+	$java -Xmx40g -jar $GATKv3_5 -nct 20 -T PrintReads -R $hg38 -I ${filepath}tempfiles/${nolane}.sorted.bam -l INFO -BQSR ${filepath}tempfiles/${nolane}.sorted.table -o ${filepath}recal_bam/${nolane}.sorted.recal.bam
 done <${filepath}samples_noLane.txt
